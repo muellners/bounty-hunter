23a8defb83f551eb075cbea6ce4f5f38327d795f
xurror <kaze.nasser@outlook.com>
2020-05-28 18:25:46 +0100

    muellners bounty hunter bot

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+  app.on('pull_request.opened', async (context) => {
+    const pr =context.payload.pull_request
+    const user = pr.user.login //Collecting Details of the person who created the PR
+
+    const issueComment = context.issue({ body: 'Hey @'+ user +'. Thanks for opening this PR!' })
+    await context.github.issues.createComment(issueComment)
+  })
+
+  app.on('pull_request.reopened', async (context) => {
+    const pr =context.payload.pull_request
+    const user = pr.user.login //Collecting Details of the person who created the PR
+
+    const issueComment = context.issue({ body: 'Hey @'+ user +'. Thanks for reopening this PR!' })
+    await context.github.issues.createComment(issueComment)
+  })
+
+  app.on('pull_request.closed', async (context) => {
+    const issueComment = context.issue({ body: 'Sad that you have to close this!' })
     await context.github.issues.createComment(issueComment)
   })
   // For more information on building apps:
